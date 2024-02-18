c6a4f381935e5a27a0317dffed2f1442e88ac8d9
mrsZemiak <majuskapappova13@gmail.com>
2024-02-18 14:04:34 +0100

    prvý commit
    toto je prvý commit

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja prvá stránka v PHP</title>
+</head>
+<body>
+<h1>Vitajte na mojej prvej stránke v PHP!</h1>
 <?php
-echo "<h1>Henlo world</h1>"
+echo "<h1>Henlo world</h1>";
+echo "Dnes je " . date("d.m.Y") . "<br>";
+echo "Aktuálny čas je " . date("H:i:s") . "<br>";
 ?>
-
+</body>
+</html>
