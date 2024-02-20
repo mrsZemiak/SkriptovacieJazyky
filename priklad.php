15a54396e9251ab59e64e7bbd80347b2a00a1e0f
mrsZemiak <majuskapappova13@gmail.com>
2024-02-20 22:01:21 +0100

    ukazkaSyntaxe

---
+++ priklad.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP príklad</title>
+</head>
+<body>
+<h1>PHP príklad</h1>
+
+<?php
+
+// **Premenné**
+$meno = "Peter";
+$vek = 30;
+$zamestnanie = "programátor";
+$jeZenaty = True;
+
+// **Podmienka**
+if ($vek >= 18) {
+    echo "<p>{$meno} je dospelý.</p>";
+} else {
+    echo "<p>{$meno} je dieťa.</p>";
+}
+
+// list
+$zvierata = array("Dog", "Cat", "Horse");
+
+echo "<ul>";
+foreach($zvierata as $item){
+    echo "<li>$item</li>";
+}
+echo "</ul>";
+// **Numerické pole**
+$cisla = array(1, 2, 3, 4, 5);
+
+// **Asociatívne pole**
+$osoba = array("meno" => "Peter", "vek" => 30, "zamestnanie" => "programátor");
+
+// **Dvojrozmerné pole**
+$mesiace = array(
+    array("január", "február", "marec"),
+    array("apríl", "máj", "jún"),
+    array("júl", "august", "september"),
+    array("október", "november", "december"),
+);
+
+// **Typy cyklov**
+
+// **Cyklus `for`**
+for ($i = 0; $i < count($cisla); $i++) {
+    echo "<p>{$cisla[$i]}</p>";
+}
+
+// **Cyklus `foreach`**
+foreach ($osoba as $kluc => $hodnota) {
+    echo "<p>{$kluc}: {$hodnota}</p>";
+}
+
+// **Cyklus `while`**
+$i = 0;
+while ($i < count($mesiace)) {
+    echo "<p>{$mesiace[$i][0]}</p>";
+    $i++;
+}
+
+// **Cyklus `do while`**
+$i = 0;
+do {
+    echo "<p>{$mesiace[$i][1]}</p>";
+    $i++;
+} while ($i < count($mesiace));
+
+?>
+</body>
+</html>
